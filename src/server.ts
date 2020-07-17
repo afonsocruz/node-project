92b00d18650b00d0e4572dd82adfd524b698ad59
afonsocruz <afonsocruz.rj@gmail.com>
2020-07-17 17:08:59 -0300

    add: debug config set

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World' });
+});
 
 app.listen(3838, () => {
   console.log('💚 Server stated on port 3838');

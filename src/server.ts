d5f34a45671bef89a16b8af6b5fa2d570c9838de
afonsocruz <afonsocruz.rj@gmail.com>
2020-07-17 13:50:58 -0300

    add: package.json scripts added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World' })
+})
+
 app.listen(3838, () => {
     console.log('💚 Server stated on port 3838')
 });
